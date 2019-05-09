10ec082423b4c9da673d088e891418ca9a3697d1
Carlos Micó <carlosmico15@gmail.com>
2019-05-09 13:23:06 +0200

    Fundamentos JavaScript

---
+++ JAVASCRIPT/FUNDAMENTOS/main.js
@@ -0,0 +1,51 @@
+// === Variables ===
+
+//     var nombre, edad;
+
+// nombre = "Carlos";
+
+// edad = nombre;
+
+// console.log("Edad: " + edad);
+
+// ===
+// Acceso a objetos ===
+
+//     var objeto = {
+//         nombre: "Carlos",
+//         apellidos: "Micó Huertas"
+//     }
+
+// console.log(objeto.nombre)
+
+// ===
+// Arrays ===
+
+//     var arr = [1, 2, 3, 4, 5]
+
+// console.log(arr.length)
+
+// ===
+// Comillas en un String ===
+
+//     var texto = "Esto es un \"String\" con comillas dentro"
+// var texto = 'Esto es un "String" con comillas dentro'
+
+
+
+// var arr = [1, 2, 3, { numero: 1 }]
+
+// var variable = 'numero'
+
+// console.log(arr[3][variable])
+
+// console.log(2 + 2)
+
+// ===
+// Casteo de Números rápido ===
+
+//     var numero = "4"
+
+// numero = +numero + 3
+
+// console.log(numero)
